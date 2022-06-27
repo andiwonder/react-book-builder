bb4f6a4cb912fef1cc184c8f47b7d275f5c2626b
andiwonder <skasliwal12@gmail.com>
2022-06-27 15:47:59 -0400

    mongo schema, env, server

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,33 @@
-import { Button } from '@material-ui/core';
+import { Button } from '@mui/material';
+import PropTypes from 'prop-types';
 import Head from 'next/head';
 
-const Index = () => (
+const propTypes = {
+  user: PropTypes.shape({
+    displayName: PropTypes.string,
+    email: PropTypes.string.isRequired,
+  }),
+};
+
+const defaultProps = {
+  user: null,
+};
+
+const Index = ({ user }) => (
   <div style={{ padding: '10px 45px' }}>
     <Head>
       <title>Index page</title>{' '}
       <meta name="description" content="This is a description of the Index page" />
     </Head>
     <p>Content on Index page</p>
+    <p>{user.email}</p>
     <Button variant="contained">MUI button</Button>
   </div>
 );
+
+Index.getInitialProps = async (ctx) => ({ user: ctx.query.user });
+
+Index.propTypes = propTypes;
+Index.defaultProps = defaultProps;
 
 export default Index;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const next = require('next');
+const mongoose = require('mongoose');
+
+const dev = process.env.NODE_ENV !== 'production';
+const MONGO_URL = process.env.MONGO_URL_TEST;
+
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(MONGO_URL, options);
 
 const port = process.env.port || 8000;
 const ROOT_URL = `http://localhost:${port}`;
 
-const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
@@ -12,7 +24,8 @@ app.prepare().then(() => {
   const server = express();
 
   server.get('/', (req, res) => {
-    res.send('Express Server');
+    const user = { email: 'skasliwal12@gmail.com' };
+    app.render(req, res, '/', { user });
   });
 
   server.get('*', (req, res) => handle(req, res));
