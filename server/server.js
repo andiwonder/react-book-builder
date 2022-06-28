721318f9246c60ec78322e1d7261beeec087afba
andiwonder <skasliwal12@gmail.com>
2022-06-28 17:14:56 -0400

    added menuWithAvatar to Header, mongo setup and connection

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
-const express = require('express');
 const next = require('next');
+const express = require('express');
 const mongoose = require('mongoose');
+const mongoSessionStore = require('connect-mongo');
 
-const dev = process.env.NODE_ENV !== 'production';
+const session = require('express-session');
+const User = require('./models/User');
+require('dotenv').config();
+
+const MongoStore = mongoSessionStore(session);
 const MONGO_URL = process.env.MONGO_URL_TEST;
+const dev = process.env.NODE_ENV !== 'production';
 
 const options = {
   useNewUrlParser: true,
@@ -23,8 +29,28 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   const server = express();
 
-  server.get('/', (req, res) => {
-    const user = { email: 'skasliwal12@gmail.com' };
+  const sess = {
+    name: process.env.SESSION_NAME,
+    secret: process.env.SESSION_SECRET,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 14 * 24 * 60 * 60, // save session 14 days
+    }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      maxAge: 14 * 24 * 60 * 60 * 1000,
+      domain: 'localhost',
+    },
+  };
+
+  server.use(session(sess));
+
+  server.get('/', async (req, res) => {
+    // const user = { email: 'skasliwal12@gmail.com' };
+    req.session.foo = 'bar';
+    const user = await User.findOne({ slug: 'skasliwal' });
     app.render(req, res, '/', { user });
   });
 
